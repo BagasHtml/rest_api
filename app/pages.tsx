98e61062fe286c3b0b5339d0a9aa2bd0a40ccaad
Bagas Html <ida.indawati@gmail.com>
2026-05-13 05:49:29 +0700

    add pages

--- app/pages.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
-      <h1>🚀 Waste Prediction API</h1>
-      <p>Backend API sudah berjalan dengan baik.</p>
-      <p>Silakan akses endpoint <code>/api/waste</code> menggunakan metode POST untuk mendapatkan data prediksi.</p>
-    </div>
-  );
-}
